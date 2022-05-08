57e314afc15b8189186d589acc3b569c2b36bc00
Naveenkumar Sivalingam <msnaveenkumar98@gmail.com>
2022-05-08 15:57:04 +0530

    task for the day

--- TypeOf_truthy_falsy.js
+++ TypeOf_truthy_falsy.js
@@ -1,1 +1,84 @@
-// Demo
+
+// 1. Write a function to find value was number or not?
+// 2. Write a function to find value was boolean or not?
+// 3. Write a function to find value was string or not?
+// 4. Write a function to find value was object or not?
+// 5. Write a function to find value was function or not?
+// 6. Write a function to find value was array or not?
+// 7. Write a function to find value was date or not?
+// 8. What is copy by value and copy by refference, write example?
+// 9. What are the values are truthy values?
+// 10. What are the values are falsy values?
+
+// function isNumber(value) {
+//   if (typeof value === "number") {
+//     console.log("TRUE");
+//   } else {
+//     console.log("FALSE");
+//   }
+// }
+// isNumber(89);
+
+// function isBoolean(value) {
+//   if (typeof value === "boolean") {
+//     console.log("TRUE");
+//   } else {
+//     console.log("FALSE");
+//   }
+// }
+// isBoolean(true);
+
+
+// function isString(value) {
+//   if (typeof value === "string") {
+//     console.log("TRUE");
+//   } else {
+//     console.log("FALSE");
+//   }
+// }
+// isString(9);
+
+// function isObject(value) {
+//   if (typeof value === "object") {
+//     console.log("TRUE");
+//   } else {
+//     console.log("FALSE");
+//   }
+// }
+// isObject({});
+
+
+// function isFunction(value) {
+//   if (typeof value === "function") {
+//     console.log("TRUE");
+//   } else {
+//     console.log("FALSE");
+//   }
+// }
+// isFunction(function(){});
+
+// function isArrayValue(value) {
+//   if (Array.isArray(value)) {
+//     console.log("TRUE");
+//   } else {
+//     console.log("FALSE");
+//   }
+// }
+// isArrayValue([]);
+
+
+// function isDate(value) {
+//   if (value instanceof Date) {
+//     console.log("TRUE");
+//   } else {
+//     console.log("FALSE");
+//   }
+// }
+// isDate( new Date(1995, 11, 17) ); 
+
+// //TRUTHY VALUES :
+// 1,new Date(),function(){},[],true,{},"hello"
+
+// //FALSY VALUES :
+// undefined, null, NaN, 0,-0, "" , false
+
